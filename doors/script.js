7fcfe10ce0b88bbde222d51954605cffc7e31b47
MarinaShemesh <marina.shemesh1@gmail.com>
2018-01-04 12:15:12 +0200

    4Jan

---
+++ doors/script.js
@@ -0,0 +1,16 @@
+console.log("js is running");
+
+  function play(){
+       const audio = document.getElementById("audio");
+       audio.play();
+                 }
+
+ function playRed(){
+       const audio = document.getElementById("audio_b");
+       audio.play();
+                 }
+
+ function playBeach(){
+       const audio = document.getElementById("audio_c");
+       audio.play();
+                 }
